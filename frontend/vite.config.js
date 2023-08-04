159857997a85d5d3cd4d5432742a386cfc5e97dd
valentin-morette <morette.valentin@gmail.com>
2023-08-04 12:09:41 +0200

    deploy2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://valentin-morette.github.io/",
+  base: "/portfoliov2/",
   plugins: [react()],
   resolve: {
     alias: {
