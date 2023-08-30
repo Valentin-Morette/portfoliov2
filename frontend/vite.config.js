69179872f0ac20e15379161a09766c0c8eaf1b59
valentin-morette <morette.valentin@gmail.com>
2023-08-30 21:00:28 +0200

    removeCNAME

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://valentin-morette.com/",
+  base: "https://Valentin-Morette.github.io/portfoliov2/",
   plugins: [react()],
   resolve: {
     alias: {
